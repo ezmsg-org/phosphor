45f13af56abed02b8ceb757a27d368a399681e86
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-02-22 10:13:44 -0500

    __version__ re-export

--- src/phosphor/__init__.py
+++ src/phosphor/__init__.py
@@ -1,5 +1,6 @@
 """GPU-accelerated real-time sweep renderer for multichannel timeseries."""
 
+from .__version__ import __version__ as __version__
 from .channel_plot import ChannelPlotWidget
 from .sweep_widget import SweepConfig, SweepWidget
 
